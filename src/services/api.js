58c9e2f3a4ed9f46875bf42b36949f3ff6d56320
Gustavo <gus.carraro@hotmail.com>
2024-12-03 09:46:37 -0300

    ajustes

--- src/services/api.js
+++ src/services/api.js
@@ -1,39 +1,39 @@
 import axios from 'axios';
 
-const API_AUTH_URL = 'http://cloud.escalasoft.com.br:8051/escalasoft/authorization';
-const API_INDICE_URL = 'http://cloud.escalasoft.com.br:8051/escalasoft/operacional/painel/IndiceAtendimentoPraca';
+// Atualize as URLs para HTTPS
+const API_AUTH_URL = 'https://cloud.escalasoft.com.br:8051/escalasoft/authorization';
+const API_INDICE_URL = 'https://cloud.escalasoft.com.br:8051/escalasoft/operacional/painel/IndiceAtendimentoPraca';
 
 let token = null;
 let tokenExpiration = null;
 
 export const getAuthToken = async () => {
-    try {
-      const credentials = btoa('gustavo.carraro:hatuna10'); // Substitua com credenciais corretas
-      const response = await axios.post(
-        API_AUTH_URL,
-        {},
-        {
-          headers: {
-            Authorization: `Basic ${credentials}`,
-          },
-        }
-      );
-  
-      // Extraindo o token corretamente do array `retorno`
-      const tokenData = response.data?.retorno?.[0]?.token;
-      if (!tokenData) {
-        throw new Error('Token não encontrado na resposta da API');
+  try {
+    const credentials = btoa('gustavo.carraro:hatuna10'); // Substitua com credenciais corretas
+    const response = await axios.post(
+      API_AUTH_URL,
+      {},
+      {
+        headers: {
+          Authorization: `Basic ${credentials}`,
+        },
       }
-  
-      token = tokenData; // Salva o token
-      tokenExpiration = new Date(Date.now() + 24 * 60 * 60 * 1000); // Define validade do token (24 horas)
-      return token;
-    } catch (error) {
-      throw new Error('Falha na autenticação');
+    );
+
+    // Extraindo o token corretamente do array `retorno`
+    const tokenData = response.data?.retorno?.[0]?.token;
+    if (!tokenData) {
+      throw new Error('Token não encontrado na resposta da API');
     }
-  };
-  
-  
+
+    token = tokenData; // Salva o token
+    tokenExpiration = new Date(Date.now() + 24 * 60 * 60 * 1000); // Define validade do token (24 horas)
+    return token;
+  } catch (error) {
+    console.error('Erro ao obter token:', error.response?.data || error.message);
+    throw new Error('Falha na autenticação');
+  }
+};
 
 export const fetchIndiceAtendimento = async (dataInicial, dataFinal) => {
   try {
@@ -41,7 +41,6 @@ export const fetchIndiceAtendimento = async (dataInicial, dataFinal) => {
     if (!token || new Date() >= tokenExpiration) {
       await getAuthToken();
     }
-
 
     const response = await axios.get(API_INDICE_URL, {
       params: {
