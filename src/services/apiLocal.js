d9ad1843cce2f7b2814f339003dc6bf8f965c055
Gustavo <gus.carraro@hotmail.com>
2025-03-14 15:55:14 -0300

    tentativa de ajuste endpoint5

--- src/services/apiLocal.js
+++ src/services/apiLocal.js
@@ -14,59 +14,60 @@ const api = axios.create({
 console.log("🚀 API Base URL:", api.defaults.baseURL); // Confirmação no console
 
 const apiLocal = {
-  getMotoristas: () => api.get("/motoristas"),
+  getMotoristas: () => api.get("/motoristas/"),
   createOrUpdateMotorista: (data) => api.post("/motoristas", data),
   deleteMotorista: (id) => api.delete(`/motoristas/${id}`),
 
-  getClientes: () => api.get("/clientes"),
+  getClientes: () => api.get("/clientes/"),
   createOrUpdateCliente: (data) => api.post("/clientes", data),
   deleteCliente: (id) => api.delete(`/clientes/${id}`),
 
-  getNomesOcorrencias: () => api.get("/nomes-ocorrencias"),
-  createOrUpdateNomeOcorrencia: (data) => api.post("/nomes-ocorrencias", data),
+  getNomesOcorrencias: () => api.get("/nomes-ocorrencias/"),
+  createOrUpdateNomeOcorrencia: (data) => api.post("/nomes-ocorrencias/", data),
   deleteNomeOcorrencia: (id) => api.delete(`/nomes-ocorrencias/${id}`),
 
-  getOcorrencias: () => api.get("/ocorrencias"),
-  createOrUpdateOcorrencia: (data) => api.post("/ocorrencias", data),
+  getOcorrencias: () => api.get("/ocorrencias/"),
+  createOrUpdateOcorrencia: (data) => api.post("/ocorrencias/", data),
   deleteOcorrencia: (id) => api.delete(`/ocorrencias/${id}`),
   getOcorrenciasFiltradas: (filters) => api.post("/ocorrencias/filtrar", filters),
   updateCobrancaAdicional: (data) => api.put("/ocorrencias/cobranca-adicional", data),
 
-  getOcorrenciasSTH: () => api.get("/ocorren-sth"), 
-  createOrUpdateOcorrenciaSTH: (data) => api.post("/ocorren-sth", data),
+  getOcorrenciasSTH: () => api.get("/ocorren-sth/"), 
+  createOrUpdateOcorrenciaSTH: (data) => api.post("/ocorren-sth/", data),
   deleteOcorrenciaSTH: (id) => api.delete(`/ocorren-sth/${id}`),
   getDashboardSTH: (filters) => api.post("/ocorren-sth/filtrar", filters),
 
-  getFaltas: () => api.get("/faltas"),
+  getFaltas: () => api.get("/faltas/"),
   getFaltasFiltradas: (filters) => api.post("/faltas/filtrar", filters),
-  createOrUpdateFalta: (data) => api.post("/faltas", data),
+  createOrUpdateFalta: (data) => api.post("/faltas/", data),
   deleteFalta: (id) => api.delete(`/faltas/${id}`),
 
-  getDestinos: () => api.get("/destinos"),
-  createOrUpdateDestino: (data) => api.post("/destinos", data),
+  getDestinos: () => api.get("/destinos/"),
+  createOrUpdateDestino: (data) => api.post("/destinos/", data),
   deleteDestino: (id) => api.delete(`/destinos/${id}`), 
 
   getControleEstoque: () => api.get("/estoque/"), 
-  createOrUpdateControleEstoque: (data) => api.post("/estoque", data),
+  createOrUpdateControleEstoque: (data) => api.post("/estoque/", data),
   deleteControleEstoque: (id) => api.delete(`/estoque/${id}`), 
   getControleEstoqueFiltrado: (filters) => api.post("/controle-estoque/filtrar", filters),
   updateSetorControleEstoque: (data) => api.put("/controle-estoque/setor", data),
 
-  getViagens: () => api.get("/viagens"),
+  getViagens: () => api.get("/viagens/"),
   getViagemByNumero: (numero_viagem) => api.get(`/viagens/${numero_viagem}`),
-  createOrUpdateViagem: (data) => api.post("/viagens", data),
+  createOrUpdateViagem: (data) => api.post("/viagens/", data),
   updateViagem: (data) => api.put("/viagens/update", data),
   deleteViagem: (numero_viagem) => api.delete(`/viagens/${numero_viagem}`),
 
   getDocumentoTransporte: (numero_cte) => api.get(`/documentos-transporte/${numero_cte}`),
 
-  getCustosFrete: (filters) => api.get("/custos-frete", { params: filters }),
-  createOrUpdateCustosFrete: (data) => api.post("/custos-frete", data),
+  getCustosFrete: (filters) => api.get("/custos-frete/"),
+  createOrUpdateCustosFrete: (data) => api.post("/custos-frete/", data),
   deleteCustosFrete: (id) => api.delete(`/custos-frete/${id}`),
 
-  getCicloPedido: () => api.get("/processamento/ciclo-pedido"),
-  getBalanceteCarraro: () => api.get("/processamento/balancete-carraro"),
-  getFechamentoOperacao: () => api.get("/processamento/fechamento-operacao"),
+  getCicloPedido: () => api.get("/processamento/ciclo-pedido/"),
+  getBalanceteCarraro: () => api.get("/processamento/balancete-carraro/"),
+  getFechamentoOperacao: () => api.get("/processamento/fechamento-operacao/"),
 };
+
 
 export default apiLocal;
